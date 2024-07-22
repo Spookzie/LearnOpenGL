9ea2c461589941547ce28c6c418eb263d7d0855e
Vansh Bansal <vb.vansh04@gmail.com>
2024-07-22 21:02:26 +0530

    Understanding & writing shaders

--- LearnOpenGL/src/Application.cpp
+++ LearnOpenGL/src/Application.cpp
@@ -1,6 +1,51 @@
-#include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();       // Convert the source string to a C-style string, can also use ' &source[0] '
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    //Error Handling
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int len;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
+        char* message = (char*)alloca(len * sizeof(char));  // We did not use malloc becuase that requires manual deallocation, while alloca does it automatically
+        glGetShaderInfoLog(id, len, &len, message);
+        std::cout << "ERROR::Application.cpp::CompileShader():: Failed to compile"<< (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+
+static unsigned int CreateShader(const std::string& vertex_shader, const std::string& fragment_shader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertex_shader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragment_shader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 
 void main()
@@ -9,7 +54,7 @@ void main()
 
     //Initializing GLFW
     if (!glfwInit())
-        std::cout << "ERROR::Application.cpp:: Failed to initialize glfw" << std::endl;
+        std::cout << "ERROR::Application.cpp::Main():: Failed to initialize glfw" << std::endl;
 
 
     //Creating Window
@@ -17,7 +62,7 @@ void main()
     if (!window)
     {
         glfwTerminate();
-        std::cout << "ERROR::Application.cpp:: Failed to create a window" << std::endl;
+        std::cout << "ERROR::Application.cpp::Main():: Failed to create a window" << std::endl;
     }
 
 
@@ -27,7 +72,7 @@ void main()
 
     //Initializing GLEW
     if (glewInit() != GLEW_OK)
-        std::cout << "ERROR:: Failed to initialize GLEW" << std::endl;
+        std::cout << "ERROR::Application.cpp::Main():: Failed to initialize GLEW" << std::endl;
 
 
     //Setting up a vertex buffer for triangle
@@ -44,6 +89,31 @@ void main()
     
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    //Source code for the vertex shader
+    std::string vertexShader =
+        "#version 330 core\n"
+        ""
+        "layout(location = 0) in vec4 position;\n"
+        ""
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+    
+    //Source code for the fragment shader
+    std::string fragmentShader =
+        "#version 330 core\n"
+        ""
+        "layout(location = 0) out vec4 color;\n"
+        ""
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
 
     //Game Loop 
@@ -63,6 +133,7 @@ void main()
         glfwPollEvents();
     }
 
+    glDeleteProgram(shader);
 
     glfwTerminate();
 }
